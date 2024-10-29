3d07f0bb8504999da3805e9326a3a5433244a16f
Eric Joldasov <bratishkaerik@landless-city.net>
2024-10-29 05:06:01 +0500

    expressive-code: re-enable `collapse-sections` plugin
    Works now after dependencies were updated.
    This reverts commit ba5a4d01e34b53002293965da93e87d397a09182.
    
    Signed-off-by: Eric Joldasov <bratishkaerik@landless-city.net>

--- ec.config.mjs
+++ ec.config.mjs
@@ -2,9 +2,9 @@
 //
 // SPDX-License-Identifier: CC0-1.0
 
-//import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
 
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
-	// plugins: [pluginCollapsibleSections()],
+	plugins: [pluginCollapsibleSections()],
 }
